a16b746261535563fa7ac56cbaae039a59f4466d
victor <victor.ryzhikov@gmail.com>
2023-12-03 16:45:13 +0000

    Add app_label

--- testProjectMagnificode/settings.py
+++ testProjectMagnificode/settings.py
@@ -47,6 +47,7 @@ CSRF_TRUSTED_ORIGINS = json.loads(get_env('CSRF_TRUSTED_ORIGINS'))
 # Application definition
 
 INSTALLED_APPS = [
+    'testProjectMagnificode',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
